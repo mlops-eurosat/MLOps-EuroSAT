93563c2562dc390fb756ba456b07d67fc21bf5fa
mspeckbacher <michael.speckbacher@outlook.de>
2026-06-09 21:33:23 +0200

    fix preprocess-data path

--- tasks.py
+++ tasks.py
@@ -39,7 +39,7 @@ def dev_requirements(ctx: Context) -> None:
 @task
 def preprocess_data(ctx: Context) -> None:
     """Preprocess data."""
-    ctx.run(f"python src/{PROJECT_NAME}/data.py data/raw data/processed", echo=True, pty=not WINDOWS)
+    ctx.run(f"python src/{PROJECT_NAME}/data.py data/raw/EuroSAT_RGB data/processed", echo=True, pty=not WINDOWS)
 
 
 @task
